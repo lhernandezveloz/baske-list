caee98b84698acb1561f05477bee01d2c6a6f9d5
Lisbel Cruz <Lcruztaveras@gmail.com>
2025-06-27 17:34:22 -0500

    Add bold titles

--- app/components/CardComponent.jsx
+++ app/components/CardComponent.jsx
@@ -2,7 +2,7 @@ function CardComponent({ title, children, footer }) {
   return (
     <div className="card">
       <div className="card-body d-flex flex-column gap-3">
-        <h5 className="card-title">{title}</h5>
+        <h5 className="card-title fw-bold">{title}</h5>
         {children}
       </div>
       <div className="card-footer">{footer}</div>
